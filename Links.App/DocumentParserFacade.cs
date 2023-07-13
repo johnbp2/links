b2b439c04f45163ddc40e9473e37c2fb00bba6c8
johnbp2 <john.johnpearson@gmail.com>
2023-07-12 22:20:50 -0500

    First commit

---
+++ Links.App/DocumentParserFacade.cs
@@ -0,0 +1,86 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Links.Application
+{
+    public static class DocumentParserFacade
+    {
+
+        public static bool IsValidWebPageFileExtension(string path)
+        {
+
+            if (path == null)
+            {
+                return false;
+            }
+            if (!Path.Exists(path))
+            {
+
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    var test = new FileInfo(path);
+                    if (!test.Exists)
+                    {
+
+                        return false;
+                    }
+                    else
+                    {
+
+                        if (test.Extension == ".html" || test.Extension == ".htm")
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // throw ex;
+                    return false;
+                }
+
+            }
+
+        }
+
+
+        private static string getFileContents(string path)
+        {
+            return File.ReadAllText(path);
+
+          
+        }
+
+
+        public static IEnumerable<Tuple<string, string>> AngleSharpParse(string filePath, out IHtmlCollection<IElement> hrefs )
+        {
+            var hrefTags = new List<Tuple<string, string>>();
+
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(getFileContents(filePath));
+            hrefs = document.QuerySelectorAll("a");
+            foreach (IElement element in document.QuerySelectorAll("a"))
+            {
+                var t = new Tuple<string, string>(element.InnerHtml, element.GetAttribute("href")?? "");
+
+                hrefTags.Add(t);
+            }
+
+            return hrefTags;
+        }
+    }
+}
---
+++ Links.App/IUsecase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Links.Application
+{
+
+    public interface IUseCase<TInput, TOutput>
+    {
+
+
+
+        TOutput Execute(TInput input);
+
+    }
+
+    public interface IUseCaseInput<TInput>
+    {
+        TInput HtmlAnchors
+        {
+            get;
+
+
+        }
+
+
+
+    }
+
+
+    public interface IUseCaseOutput<TOutput>
+    {
+        TOutput Html
+        {
+            get;
+
+        }
+
+
+
+    }
+}
